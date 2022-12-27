eb47e2c2f1221c2e85a1e4d6d265986879f9f2b0
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-12-27 02:21:00 -0300

    Feature: cambiar de Celcius a Fahrenheit y viceversa

--- src/Components/WeatherGlobal.js
+++ src/Components/WeatherGlobal.js
@@ -14,6 +14,9 @@ const WeatherGlobal = () => {
     const [forecastData, setForecastData] = useState(null);
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    // Para convertir temperaturas
+    const [temperature, setTemperature] = useState(20); // temperatura inicial en grados Celsius
+    const [isCelsius, setIsCelsius] = useState(true); // indica si la temperatura está en grados Celsius
 
     const fetchData = async searchTerm => {
         setIsLoading(true);
@@ -55,7 +58,7 @@ const WeatherGlobal = () => {
                             value={searchTerm}
                             onChange={e => setSearchTerm(e.target.value)}
                         />
-                        <button type='submit'>Buscar</button>   
+                        <button type='submit'>Buscar</button>
                     </form>
                 </div>
             </div>
@@ -65,7 +68,7 @@ const WeatherGlobal = () => {
     if (!weatherData || !forecastData) {
         return null;
     }
-
+    // Para obtener la fecha segun el pais
     const currentDate = new Date();
     currentDate.setTime(currentDate.getTime() + weatherData.timezone * 1000);
     const localDate = currentDate.toLocaleDateString();
@@ -74,6 +77,8 @@ const WeatherGlobal = () => {
     const celsius = weatherData.main.temp - 273.15;
     const maxTemperatureCelsius = weatherData.main.temp_max - 273.15;
     const minTemperatureCelsius = weatherData.main.temp_min - 273.15;
+    const maxTemperatureFahrenheit = maxTemperatureCelsius * 9 / 5 + 32;
+    const minTemperatureFahrenheit = minTemperatureCelsius * 9 / 5 + 32;
     const iconName = weatherData.weather[0].icon;
     const iconUrl = `https://openweathermap.org/img/wn/${iconName}@2x.png`;
 
@@ -161,9 +166,19 @@ const WeatherGlobal = () => {
             return isNaN(temp) ? acc : acc + temp;
         }, 0) / firstDayData.length;
 
-    
 
-    
+    // función que convierte la temperatura de grados Celsius a grados Fahrenheit y actualiza el estado
+    const handleConvertTemperature = () => {
+        if (isCelsius) {
+            setTemperature(temperature * 9 / 5 + 32);
+            setIsCelsius(false);
+        } else {
+            setTemperature((temperature - 32) * 5 / 9);
+            setIsCelsius(true);
+        }
+    };
+
+
     //Obtener la hora para definir si es mañana,tarde o noche
     const rectangleClass =
         currentHour >= 6 && currentHour < 12 ? 'morning' :
@@ -174,6 +189,7 @@ const WeatherGlobal = () => {
             <div className={`rectangle ${rectangleClass}`}>
                 <div className="top">
                     {/* Buscador de ciudades */}
+                    <button onClick={handleConvertTemperature}>Convertir</button>
                     <div className="weather-card__search">
                         <form onSubmit={handleSubmit}>
                             <input type="text" placeholder='Ingresa una ciudad' value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
@@ -189,11 +205,13 @@ const WeatherGlobal = () => {
                         <div >{weatherData.weather[0].main} <img src={iconUrl} alt='tiempo-Icon' /> </div>
                     </h1>
                     <h3>
-                        <div className='temp'>{Math.round(celsius)}°C</div>
+                        <div className="temp">{Math.round(temperature)}°{isCelsius ? 'C' : 'F'}</div>
                         <div>
                             <div>
-                                {/* Valores del dia */}
-                                Mín {Math.round(minTemperatureCelsius)}°C - Máx {Math.round(maxTemperatureCelsius)}°C</div>
+                                {/* Valores del día */}
+                                Mín {Math.round(isCelsius ? minTemperatureCelsius : minTemperatureFahrenheit)}°{isCelsius ? 'C' : 'F'} - Máx {Math.round(isCelsius ? maxTemperatureCelsius : maxTemperatureFahrenheit)}°{isCelsius ? 'C' : 'F'}
+                            </div>
+
                             <div>Humedad: {weatherData.main.humidity}%</div>
                             <div>Viento: {weatherData.wind.speed} m/s</div>
                             <div>Presion: {weatherData.main.pressure} hPa</div>
@@ -207,43 +225,52 @@ const WeatherGlobal = () => {
             <div className="bottom">
                 <div className="wrapper">
                     <ul className="forecast">
-                        
                         <li>
                             <span>{firstDay.toLocaleDateString('es-ES', { day: 'numeric', month: 'numeric' })}</span>
                             <span className="lnr lnr-cloud condition">
-                                <span className="temp">{firstDayTemperature.toFixed(1)}<span
-                                    className="temp-type">° C</span></span>
+                                <span className="temp">
+                                    {isCelsius ? firstDayTemperature.toFixed(1) : (firstDayTemperature * 9 / 5 + 32).toFixed(1)}
+                                    <span className="temp-type">°{isCelsius ? 'C' : 'F'}</span>
+                                </span>
+
                             </span>
                         </li>
                         <li>
                             <span>{secondDay.toLocaleDateString('es-ES', { day: 'numeric', month: 'numeric' })}</span>
                             <span className="lnr lnr-cloud condition">
-                                <span className="temp">{secondDayTemperature.toFixed(1)}<span
-                                    className="temp-type">° C</span></span>
+                                <span className="temp">
+                                    {isCelsius ? secondDayTemperature.toFixed(1) : (secondDayTemperature * 9 / 5 + 32).toFixed(1)}
+                                    <span className="temp-type">°{isCelsius ? 'C' : 'F'}</span>
+                                </span>
                             </span>
                         </li>
                         <li>
                             <span>{thirdDay.toLocaleDateString('es-ES', { day: 'numeric', month: 'numeric' })}</span>
                             <span className="lnr lnr-cloud condition">
-                                <span className="temp">{thirdDayTemperature.toFixed(1)}<span
-                                    className="temp-type">° C</span></span>
+                                <span className="temp">
+                                    {isCelsius ? thirdDayTemperature.toFixed(1) : (thirdDayTemperature * 9 / 5 + 32).toFixed(1)}
+                                    <span className="temp-type">°{isCelsius ? 'C' : 'F'}</span>
+                                </span>
                             </span>
                         </li>
                         <li>
                             <span>{fourthDay.toLocaleDateString('es-ES', { day: 'numeric', month: 'numeric' })}</span>
                             <span className="lnr lnr-cloud condition">
-                                <span className="temp">{fourthDayTemperature.toFixed(1)}<span
-                                    className="temp-type">° C</span></span>
+                                <span className="temp">
+                                    {isCelsius ? fourthDayTemperature.toFixed(1) : (fourthDayTemperature * 9 / 5 + 32).toFixed(1)}
+                                    <span className="temp-type">°{isCelsius ? 'C' : 'F'}</span>
+                                </span>
                             </span>
                         </li>
                         <li>
                             <span>{fifthDay.toLocaleDateString('es-ES', { day: 'numeric', month: 'numeric' })}</span>
                             <span className="lnr lnr-cloud condition">
-                                <span className="temp">{fifthDayTemperature.toFixed(1)}<span
-                                    className="temp-type">° C</span></span>
+                                <span className="temp">
+                                    {isCelsius ? fifthDayTemperature.toFixed(1) : (fifthDayTemperature * 9 / 5 + 32).toFixed(1)}
+                                    <span className="temp-type">°{isCelsius ? 'C' : 'F'}</span>
+                                    </span>
                             </span>
                         </li>
-
                     </ul>
                 </div>
             </div>
--- src/Components/WeatherLocal.js
+++ src/Components/WeatherLocal.js
@@ -7,6 +7,10 @@ const WeatherLocal = () => {
   const [forecastData, setForecastData] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  // Para convertir temperaturas
+  const [temperature, setTemperature] = useState(20); // temperatura inicial en grados Celsius
+  const [isCelsius, setIsCelsius] = useState(true); // indica si la temperatura está en grados Celsius
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -56,8 +60,11 @@ const WeatherLocal = () => {
 
   const today = new Date().toLocaleDateString();
   const celsius = weatherData.main.temp - 273.15;
+
   const maxTemperatureCelsius = weatherData.main.temp_max - 273.15;
   const minTemperatureCelsius = weatherData.main.temp_min - 273.15;
+  const maxTemperatureFahrenheit = maxTemperatureCelsius * 9 / 5 + 32;
+  const minTemperatureFahrenheit = minTemperatureCelsius * 9 / 5 + 32;
   const iconName = weatherData.weather[0].icon;
   const iconUrl = `https://openweathermap.org/img/wn/${iconName}@2x.png`;
 
@@ -147,6 +154,17 @@ const WeatherLocal = () => {
       return isNaN(temp) ? acc : acc + temp;
     }, 0) / firstDayData.length;
 
+  // función que convierte la temperatura de grados Celsius a grados Fahrenheit y actualiza el estado
+  const handleConvertTemperature = () => {
+    if (isCelsius) {
+      setTemperature(temperature * 9 / 5 + 32);
+      setIsCelsius(false);
+    } else {
+      setTemperature((temperature - 32) * 5 / 9);
+      setIsCelsius(true);
+    }
+  };
+
   //Obtener la hora para definir si es mañana,tarde o noche
   const currentHour = new Date().getHours();
   const rectangleClass =
@@ -159,6 +177,7 @@ const WeatherLocal = () => {
       <div className={`rectangle ${rectangleClass}`}>
         <br></br>
         <div className="top">
+          <button onClick={handleConvertTemperature}>Convertir</button>
           <h1>
             {/* colocar ciudad y pais */}
             <div>{weatherData.name}, {weatherData.sys.country}</div>
@@ -167,11 +186,12 @@ const WeatherLocal = () => {
             <div >{weatherData.weather[0].main} <img src={iconUrl} alt='tiempo-Icon' /> </div>
           </h1>
           <h3>
-            <div className='temp'>{Math.round(celsius)}°C</div>
+            <div className="temp">{Math.round(temperature)}°{isCelsius ? 'C' : 'F'}</div>
             <div>
               <div>
-                {/* Valores del dia */}
-                Mín {Math.round(minTemperatureCelsius)}°C - Máx {Math.round(maxTemperatureCelsius)}°C</div>
+                {/* Valores del día */}
+                Mín {Math.round(isCelsius ? minTemperatureCelsius : minTemperatureFahrenheit)}°{isCelsius ? 'C' : 'F'} - Máx {Math.round(isCelsius ? maxTemperatureCelsius : maxTemperatureFahrenheit)}°{isCelsius ? 'C' : 'F'}
+              </div>
               <div>Humedad: {weatherData.main.humidity}%</div>
               <div>Viento: {weatherData.wind.speed} m/s</div>
               <div>Presion: {weatherData.main.pressure} hPa</div>
@@ -185,39 +205,51 @@ const WeatherLocal = () => {
       <div className="bottom">
         <div className="wrapper">
           <ul className="forecast">
+
             <li>
               <span>{firstDay.toLocaleDateString('es-ES', { day: 'numeric', month: 'numeric' })}</span>
               <span className="lnr lnr-cloud condition">
-                <span className="temp">{firstDayTemperature.toFixed(1)}<span
-                  className="temp-type">° C</span></span>
+                <span className="temp">
+                  {isCelsius ? firstDayTemperature.toFixed(1) : (firstDayTemperature * 9 / 5 + 32).toFixed(1)}
+                  <span className="temp-type">°{isCelsius ? 'C' : 'F'}</span>
+                </span>
+
               </span>
             </li>
             <li>
               <span>{secondDay.toLocaleDateString('es-ES', { day: 'numeric', month: 'numeric' })}</span>
               <span className="lnr lnr-cloud condition">
-                <span className="temp">{secondDayTemperature.toFixed(1)}<span
-                  className="temp-type">° C</span></span>
+                <span className="temp">
+                  {isCelsius ? secondDayTemperature.toFixed(1) : (secondDayTemperature * 9 / 5 + 32).toFixed(1)}
+                  <span className="temp-type">°{isCelsius ? 'C' : 'F'}</span>
+                </span>
               </span>
             </li>
             <li>
               <span>{thirdDay.toLocaleDateString('es-ES', { day: 'numeric', month: 'numeric' })}</span>
               <span className="lnr lnr-cloud condition">
-                <span className="temp">{thirdDayTemperature.toFixed(1)}<span
-                  className="temp-type">° C</span></span>
+                <span className="temp">
+                  {isCelsius ? thirdDayTemperature.toFixed(1) : (thirdDayTemperature * 9 / 5 + 32).toFixed(1)}
+                  <span className="temp-type">°{isCelsius ? 'C' : 'F'}</span>
+                </span>
               </span>
             </li>
             <li>
               <span>{fourthDay.toLocaleDateString('es-ES', { day: 'numeric', month: 'numeric' })}</span>
               <span className="lnr lnr-cloud condition">
-                <span className="temp">{fourthDayTemperature.toFixed(1)}<span
-                  className="temp-type">° C</span></span>
+                <span className="temp">
+                  {isCelsius ? fourthDayTemperature.toFixed(1) : (fourthDayTemperature * 9 / 5 + 32).toFixed(1)}
+                  <span className="temp-type">°{isCelsius ? 'C' : 'F'}</span>
+                </span>
               </span>
             </li>
             <li>
               <span>{fifthDay.toLocaleDateString('es-ES', { day: 'numeric', month: 'numeric' })}</span>
               <span className="lnr lnr-cloud condition">
-                <span className="temp">{fifthDayTemperature.toFixed(1)}<span
-                  className="temp-type">° C</span></span>
+                <span className="temp">
+                  {isCelsius ? fifthDayTemperature.toFixed(1) : (fifthDayTemperature * 9 / 5 + 32).toFixed(1)}
+                  <span className="temp-type">°{isCelsius ? 'C' : 'F'}</span>
+                </span>
               </span>
             </li>
           </ul>
